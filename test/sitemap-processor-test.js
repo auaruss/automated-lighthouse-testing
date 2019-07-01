5f4da680aed56321fa030bf812c88f5f3d1d4fb5
Austin Russell <auaruss@iu.edu>
2019-07-01 10:11:55 -0400

    tests fail properly for sitemap-processor

--- test/sitemap-processor-test.js
+++ test/sitemap-processor-test.js
@@ -3,16 +3,29 @@
 const assert = require("assert");
 const sitemapProcessor = require("../src/sitemap-processor");
 
-describe("utils", function() {
-  describe("buildPageList", function() {
+
+describe("utils", () => {
+  describe("buildPageList", () => {
     it("should exist", () => {
       assert.notEqual(sitemapProcessor.buildPageList, undefined);
     });
+
+    it("should return an array of strings", () => {
+      const pageList = sitemapProcessor.buildPageList();
+      assert.equal(typeof pageList, 'object');
+      assert.equal(typeof pageList[0], 'string');
+    });
   });
 
-  describe("parseXML", function() {
+  describe("parseXML", () => {
     it("should exist", () => {
       assert.notEqual(sitemapProcessor.parseXML, undefined);
     });
+
+    it("should return an array of strings", () => {
+      const pageList = sitemapProcessor.parseXML();
+      assert.equal(typeof pageList, 'object');
+      assert.equal(typeof pageList[0], 'string');
+    })
   });
 });
